53cee00d1f675f4caf67b254b2029c2ec71568ab
Istvan Steiner <istvan.steiner@ericsson.com>
2015-05-03 11:26:21 +0200

    Some refactorings

--- copyDirToPendrive.sh
+++ copyDirToPendrive.sh
@@ -4,7 +4,8 @@
 # mp3 file copier in order to preserve the file order on the pendrive
 # It simply creates the directory and copies the fies one by one
 # This method prevents mixing of files in the target directory since the shell puts the files onto the media one by one in the order the shell wants
-# Usage: copyDirToPendrive.sh <sourceDir> <targetDir>
+# Usage: copyDirToPendrive.sh <sourceDir> <targetParentDir>
+# The directory <sourceDir> will be copied under <targetParentDir>
 
 # Version history
 # V1.0 - 09.11.2011 - First trial
@@ -12,26 +13,31 @@
 # V1.2 - 20.11.2011 - Source directory basename in order to use with full path
 # Further versions in GIT
 
+function printErrorAndUsage {
+  echo "${1}"
+  echo "Usage: `basename ${0}` <sourceDir> <targetParentDir>"
+  exit 1
+}
+
+
+# Main
+
 if [ $# -ne 2 ]
 then
-  echo "Insufficient number of parameters"
-  echo "Usage: `basename $0` <sourceDir> <targetDir>"
-  exit 1
+  printErrorAndUsage "Insufficient number of parameters."
 fi
 
-echo $1 $2
-
-if [ -d "$1" ] && [ -d "$2" ]
+if [ -d "${1}" ] && [ -d "${2}" ]
 then
-  sourceDir=`basename "$1"`
-  echo "Copying..."
-  mkdir "$2"/"$sourceDir"
-  cp -p "$1"/* "$2"/"$sourceDir"/
+  sourceDir=`basename "${1}"`
+  if ! [ -d "${2}"/"${sourceDir}" ]
+  then
+    mkdir "${2}"/"${sourceDir}"
+  fi
+
+  echo "Copying directory '${1}' under ${2} ..."
+  cp -p "${1}"/* "${2}"/"${sourceDir}"/
   echo "Ready."
-  echo "The files just copied are (directory order):"
-  ls -fl "$2"/"$sourceDir"
 else
-  echo "$1 or $2 is not a directory"
-  echo "Usage: `basename $0` <sourceDir> <targetDir>"
-  exit 1
+  printErrorAndUsage "${1} or ${2} is not a directory."
 fi
