745cb56de6aaf077f312a488e8f26186cf056eb2
Istvan Steiner <istvan.steiner@ericsson.com>
2015-05-03 18:16:04 +0200

    Add unlimited directory in directory feature. Add ToDo.txt.

--- copyDirToPendrive.sh
+++ copyDirToPendrive.sh
@@ -4,6 +4,7 @@
 # mp3 file copier in order to preserve the file order on the pendrive
 # It simply creates the directory and copies the fies one by one
 # This method prevents mixing of files in the target directory since the shell puts the files onto the media one by one in the order the shell wants
+
 # Usage: copyDirToPendrive.sh <sourceDir> <targetParentDir>
 # The directory <sourceDir> will be copied under <targetParentDir>
 
@@ -13,10 +14,13 @@
 # V1.2 - 20.11.2011 - Source directory basename in order to use with full path
 # Further versions in GIT
 
+
+# Functions
+
 function printErrorAndUsage {
-  echo "${1}"
-  echo "Usage: `basename ${0}` <sourceDir> <targetParentDir>"
-  exit 1
+    echo "${1}"
+    echo "Usage: `basename ${0}` <sourceDir> <targetParentDir>"
+    exit 1
 }
 
 
@@ -24,20 +28,30 @@ function printErrorAndUsage {
 
 if [ $# -ne 2 ]
 then
-  printErrorAndUsage "Insufficient number of parameters."
+    printErrorAndUsage "Insufficient number of parameters."
 fi
 
-if [ -d "${1}" ] && [ -d "${2}" ]
+if ! [ -d "${1}" ] || ! [ -d "${2}" ]
 then
-  sourceDir=`basename "${1}"`
-  if ! [ -d "${2}"/"${sourceDir}" ]
-  then
-    mkdir "${2}"/"${sourceDir}"
-  fi
-
-  echo "Copying directory '${1}' under ${2} ..."
-  cp -p "${1}"/* "${2}"/"${sourceDir}"/
-  echo "Ready."
-else
-  printErrorAndUsage "${1} or ${2} is not a directory."
+    printErrorAndUsage "${1} or ${2} are not directories."
 fi
+
+sourceDir=`basename "${1}"`
+targetDir="${2}"/"${sourceDir}"
+
+if ! [ -d "${targetDir}" ]
+then
+    mkdir "${targetDir}"
+fi
+
+for actFile in "${1}"/*
+do
+    if [ -d "${actFile}" ]
+    then
+        "${0}" "${actFile}" "${targetDir}"
+    elif [ -f "${actFile}" ]
+    then
+        echo "Copying file ${actFile} ${targetDir}/"
+        cp -p "${actFile}" "${targetDir}"/
+    fi
+done
